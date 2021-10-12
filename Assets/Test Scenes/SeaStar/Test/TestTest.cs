9209426aee42491b9c43473c00aa7919adccfb4c
JeonSeaStar <88014706+JeonSeaStar@users.noreply.github.com>
2021-10-12 19:55:58 +0900

    *연동 테스트

--- Assets/Test Scenes/SeaStar/Test/TestTest.cs
+++ Assets/Test Scenes/SeaStar/Test/TestTest.cs
@@ -55,6 +55,7 @@ public class TestTest : MonoBehaviour
         rigid = GetComponent<Rigidbody2D>();
         SelectChar = Char[0];
         ChangeCahr();
+        Debug.Log("dddd");
     }
 
     void FixedUpdate()
