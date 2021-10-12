090365d7acd08080488887af577652a181770c8c
dy990320 <dy990320@naver.com>
2021-10-12 20:21:16 +0900

    1

--- Assets/Test Scenes/DY/Monstertest/scripts/Firefly_monster/Firefly_Dead.cs
+++ Assets/Test Scenes/DY/Monstertest/scripts/Firefly_monster/Firefly_Dead.cs
@@ -16,7 +16,7 @@ public class Firefly_Dead : StateMachineBehaviour
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-
+        //아직 죽는 모션이 없음
     }
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
