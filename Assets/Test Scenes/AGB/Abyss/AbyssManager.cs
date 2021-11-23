29181340648bede0ff1103841f1b5002ea19de05
Kang <dkdlfhqht7895@naver.com>
2021-11-24 01:27:42 +0900

    ㅁㄴㅇ

--- Assets/Test Scenes/AGB/Abyss/AbyssManager.cs
+++ Assets/Test Scenes/AGB/Abyss/AbyssManager.cs
@@ -214,7 +214,7 @@ public class AbyssManager : MonoBehaviour
         set
         {
             abyssGage = value;
-            StateManager.state.AbyssGage = abyssGage; юс╫ц
+            StateManager.state.AbyssGage = abyssGage; 
         }
     }
 
