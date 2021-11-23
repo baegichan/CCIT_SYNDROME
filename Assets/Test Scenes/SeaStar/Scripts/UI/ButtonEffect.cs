ab6cf5636c01ad9105cacfcbcddb8610a4edbdbe
JeonSeaStar <88014706+JeonSeaStar@users.noreply.github.com>
2021-11-24 01:21:35 +0900

    Character 재수정

---
+++ Assets/Test Scenes/SeaStar/Scripts/UI/ButtonEffect.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ButtonEffect : MonoBehaviour, IPointerEnterHandler, IPointerClickHandler, IPointerExitHandler
+{
+    public Sprite Default;
+    public Sprite Hover;
+    public Sprite Disable;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+
+    }
+}
