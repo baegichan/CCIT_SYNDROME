f9eed734ff18316d46323e937b97e8e03a2c873d
tuost <fldh753@gmail.com>
2021-11-25 14:57:48 +0900

    Warring!

--- Assets/Test Scenes/SeaStar/Scripts/Character/Char_Eden.cs
+++ Assets/Test Scenes/SeaStar/Scripts/Character/Char_Eden.cs
@@ -34,9 +34,10 @@ public class Char_Eden : MonoBehaviour
     public GameObject A_One;
     public GameObject A_Two;
     public GameObject A_Three;
-
     [Header("라이트 리스트")]
     public GameObject PharaoLight;
+    public GameObject SwordLight;
+    public GameObject AxeLight;
 
     public Animator Ani;
     Rigidbody2D rigid;
@@ -107,16 +108,19 @@ public class Char_Eden : MonoBehaviour
     }
 
     void SwordAttackEffect1() { AttackEffect(One); }
-
     void SwordAttackEffect2() { AttackEffect(Two); }
-
     void SwordAttackEffect3() { AttackEffect(Three); Two.SetActive(false); }
-
     void AxeAttackEffect1() { AttackEffect(A_One); }
-
     void AxeAttackEffect2() { AttackEffect(A_Two); }
-
     void AxeAttackEffect3() { AttackEffect(A_Three); A_Two.SetActive(false); }
+
+    void SwordLightOn() { SwordLight.SetActive(true); }
+    void SwordLightOff() { SwordLight.SetActive(false); }
+   
+    void AxeLightOn() { AxeLight.SetActive(true); }
+    void AxeLightOff() { AxeLight.SetActive(false); }
+
+
     void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
