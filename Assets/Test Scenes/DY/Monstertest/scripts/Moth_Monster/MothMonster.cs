082f46de52c5e02844b0315cc31f4801a56d21c7
dy990320 <dy990320@naver.com>
2021-11-25 17:49:45 +0900

    보스 몬스터 ver.2 + 2D 라이트 적용 애니메이션

--- Assets/Test Scenes/DY/Monstertest/scripts/Moth_Monster/MothMonster.cs
+++ Assets/Test Scenes/DY/Monstertest/scripts/Moth_Monster/MothMonster.cs
@@ -96,8 +96,8 @@ public class MothMonster : Character
         first = transform.position;
         if (atkDelay >= 0)
             atkDelay -= Time.deltaTime;
-        //Up();
-        //Down();
+        Up();
+        Down();
         if (Hp_Current <= 0)
         {
             Dead = true;
@@ -190,7 +190,7 @@ public class MothMonster : Character
         RaycastHit2D upcheck = Physics2D.Raycast(upCheck.position, Vector2.up, 0.2f);
         if (upcheck.collider == true)
         {
-            rb.AddForce(transform.up * -1f);
+            rb.AddForce(transform.up * -5f);
         }
     }
     public void Down()
@@ -198,7 +198,7 @@ public class MothMonster : Character
         RaycastHit2D downcheck = Physics2D.Raycast(downCheck.position, Vector2.down, 0.2f);
         if (downcheck.collider == true)
         {
-            rb.AddForce(transform.up * 1f);
+            rb.AddForce(transform.up * 5f);
         }
     }
 
