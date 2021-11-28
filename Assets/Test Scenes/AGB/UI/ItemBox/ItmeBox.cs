acd9886939fb1e4347f770a1a56c537f54916db6
AGB <60732444+GrandChickenMaster@users.noreply.github.com>
2021-11-28 17:14:55 +0900

    수정

--- Assets/Test Scenes/AGB/UI/ItemBox/ItmeBox.cs
+++ Assets/Test Scenes/AGB/UI/ItemBox/ItmeBox.cs
@@ -4,6 +4,34 @@ using UnityEngine;
 
 public class ItmeBox : MonoBehaviour
 {
+    bool isOpen = false;
+    public Animation ani;
     // Start is called before the first frame update
-   
+
+    bool isPlayer = false;
+    private void Update()
+    {
+      
+        if (Input.GetKeyDown(KeyCode.F) && !isOpen && isPlayer)
+        {
+            Debug.Log("¿­·È´ç±ú");
+            isOpen = true;
+            ani.Play("ItemBox");
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Player") { isPlayer = true; }
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.tag == "Player") { isPlayer = false; }
+    }
+
+    void OpenItmeBox()
+    {
+        
+    }
 }
