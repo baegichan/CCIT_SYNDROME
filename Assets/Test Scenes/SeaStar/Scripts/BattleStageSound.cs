e652e67428231b5f90fadde81d13469e3fc1b3a2
baegichan <46126246+baegichan@users.noreply.github.com>
2021-11-30 06:45:38 +0900

    SAveMapData

---
+++ Assets/Test Scenes/SeaStar/Scripts/BattleStageSound.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleStageSound : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        SoundManager.BGLoop("BattleField");
+    }
+
+    // Update is called once per frame
+  
+}
