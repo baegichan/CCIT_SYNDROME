fda6fa9fc66644dae00ba5cdc32bb979ba026668
Kang <dkdlfhqht7895@naver.com>
2021-11-30 02:29:17 +0900

    보스 사운드 작업중

---
+++ Assets/Boss_Start_BGM.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss__Start_BGM : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        SoundManager.BGLoop("BattleBGM");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+
+}
