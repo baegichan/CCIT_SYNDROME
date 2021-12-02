8554ff3ffb55d5ba656057acab407da47aa10d9a
dy990320 <dy990320@naver.com>
2021-12-02 13:25:41 +0900

    update

--- Assets/Test Scenes/BGC/Scripts/MapData.cs
+++ Assets/Test Scenes/BGC/Scripts/MapData.cs
@@ -392,16 +392,16 @@ public class MapData : ScriptableObject
         for(int i=0; i<BG.Length;i++)
         {
 
-           GameObject MapPrefab =PrefabUtility.SaveAsPrefabAsset(grid.transform.GetChild(i).gameObject,"Assets/Test Scenes/BGC/TileMap/TileMaps/"+BG[i].TilemapName+ ".prefab");
-            BG[i].Tilemap = MapPrefab;
+         //  GameObject MapPrefab =PrefabUtility.SaveAsPrefabAsset(grid.transform.GetChild(i).gameObject,"Assets/Test Scenes/BGC/TileMap/TileMaps/"+BG[i].TilemapName+ ".prefab");
+         //   BG[i].Tilemap = MapPrefab;
             //추가로 이벤트도 저장해야됨
             
         }
-        Map_Event = PrefabUtility.SaveAsPrefabAsset(Event, "Assets/Test Scenes/BGC/Event/Events/" + this.name + "_Event" + ".prefab");
+      //  Map_Event = PrefabUtility.SaveAsPrefabAsset(Event, "Assets/Test Scenes/BGC/Event/Events/" + this.name + "_Event" + ".prefab");
     }
     public void Save_Event(GameObject Event)
     {
-        Map_Event = PrefabUtility.SaveAsPrefabAsset(Event, "Assets/Test Scenes/BGC/Event/Events/" + this.name + "_Event" + ".prefab");
+   //     Map_Event = PrefabUtility.SaveAsPrefabAsset(Event, "Assets/Test Scenes/BGC/Event/Events/" + this.name + "_Event" + ".prefab");
     }
 
     public Sprite Get_Sprite(int index)
