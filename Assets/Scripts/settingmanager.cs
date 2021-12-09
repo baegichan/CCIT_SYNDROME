a1ccbd2090f7e177b9aa6206bae90676a1e1feb8
baegichan <46126246+baegichan@users.noreply.github.com>
2021-12-09 16:00:27 +0900

    MapUpdate and Map 6-2 Fixed

--- Assets/Scripts/settingmanager.cs
+++ Assets/Scripts/settingmanager.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 using UnityEngine.Audio;
 public class settingmanager : MonoBehaviour
 {
-    //public static settingmanager GM;
+    public static settingmanager SM;
     public GameObject CAN;
     public GameObject ExitPanel;
     public GameObject[] button_ar;
@@ -67,6 +67,7 @@ public class settingmanager : MonoBehaviour
 
     private static settingmanager _state;
 
+    
     public static settingmanager GM
     {
         get
@@ -82,24 +83,27 @@ public class settingmanager : MonoBehaviour
             return _state;
         }
     }
+    private void instance()
+    {
+    if(SM==null)
+    {
+            SM = this;
+    }
+    else
+    {
+            Destroy(gameObject);
+    }
+    }
 
     private void Awake()
     {
-        PlayerPrefs.DeleteAll();
+
+
+        instance();
+        //PlayerPrefs.DeleteAll();
         defalutsetter();
 
         update_keycode();
-
-        if (_state == null)
-        {
-            _state = this;
-        }
-        // 인스턴스가 존재하는 경우 새로생기는 인스턴스를 삭제한다.
-        else if (_state != this)
-        {
-            Destroy(gameObject);
-        }
-        // 아래의 함수를 사용하여 씬이 전환되더라도 선언되었던 인스턴스가 파괴되지 않는다.
         DontDestroyOnLoad(gameObject);
     }
 
