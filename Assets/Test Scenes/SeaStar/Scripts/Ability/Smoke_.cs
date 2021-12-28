49727fe73b90524b80990d1cb55d027f8ced6cf0
tuost <fldh753@gmail.com>
2021-12-28 13:18:38 +0900

    검은안개 작업중

--- Assets/Test Scenes/SeaStar/Scripts/Ability/Smoke_.cs
+++ Assets/Test Scenes/SeaStar/Scripts/Ability/Smoke_.cs
@@ -5,43 +5,54 @@ using UnityEngine;
 public class Smoke_ : MonoBehaviour
 {
     public Character CT;
+    public GameObject ES;
+    public float Speed;
     public Vector2 Dir;
     public Vector2 PP;
-    public Vector2 Die;
+    public float Dis;
+    public float Distance;
 
     void Update()
     {
-        transform.Translate(Dir * 10 * Time.deltaTime);
-
-        float Dis = Vector2.Distance(PP, transform.position);
-
-        if (Dis >= 10)
+        transform.Translate(Dir * Speed * Time.deltaTime);
+        Dis = Vector2.Distance(PP, transform.position);
+        if (Dis >= Distance)
         {
-            Explosion();
+            Invoke("Explosion", 0.005f);
+            Delete();
         }
     }
 
     public void Explosion()
     {
         Collider2D[] Boom = Physics2D.OverlapCircleAll(transform.position, 1);
-        if(CT != null)
-            CT.Damage(Char_Parent.ply.AM.DarkSmokeAP[Char_Parent.ply.ActiveAbility.Enhance]);
-
-        Destroy(this.gameObject);
+        foreach (Collider2D Current in Boom)
+        {
+            Instantiate(ES, transform.position, Quaternion.identity);
+            ES.GetComponent<ParticleSystem>().Play();
+            Current.GetComponent<Character>().Damage(Char_Parent.ply.AM.DarkSmokeExplosionAP[Char_Parent.ply.ActiveAbility.Enhance]);
+            Delete();
+        }
     }
-
+    void Delete()
+    {
+        Destroy(gameObject);
+        Destroy(ES);
+    }
     private void OnDrawGizmos()
     {
         Gizmos.color = new Color(1, 0, 0);
-        Gizmos.DrawWireSphere(transform.position, 2);
+        Gizmos.DrawWireSphere(transform.position, 1);
     }
     LayerMask Layer;
     void OnTriggerEnter2D(Collider2D col)
     {
-        if (col.tag != "Player")
+        if (col.gameObject.tag != "Player")
         {
-            CT = col.GetComponent<Character>();
+            CT = col.gameObject.GetComponent<Character>();
+            CT.Damage(Char_Parent.ply.AM.DarkSmokeAP[Char_Parent.ply.ActiveAbility.Enhance]);
             Explosion();
+            Delete();
         }
     }
 }
